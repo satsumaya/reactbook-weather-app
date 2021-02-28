8af6d66387315b5adc1f709b2427223b88a4d5d1
mod728 <vinnybeach712@yahoo.co.jp>
2021-03-01 08:56:26 +0900

    Added some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
 import { useState } from "react";
 import axios from "axios";
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
+import Loading from "./components/Loading";
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [city, setCity] = useState("");
   const [results, setResults] = useState({
       country: "",
@@ -17,6 +18,7 @@ function App() {
   });
   const getWeather = (e) => {
       e.preventDefault();
+      setLoading(true);
       axios.get(`https://api.weatherapi.com/v1/current.json?key=b61340xxxxxyyyyyyyy12102&q=${city}&aqi=no`)
            .then(res => {
               setResults({
@@ -25,19 +27,21 @@ function App() {
                   temperature: res.data.current.temp_c,
                   conditionText: res.data.current.condition.text,
                   icon: res.data.current.condition.icon
-              })
+              });
+              setCity("");
+              setLoading(false);
            })
+           .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"))
   }
   return (
     <div className="wrapper">
         <div className="container">
             <Title />
             <Form getWeather={getWeather} city={city} setCity={setCity} />
-            <Results results={results} />      
+            {loading ? <Loading /> : <Results results={results} />}  
         </div>
     </div>
   );
-};
+}
 
 export default App;
-
